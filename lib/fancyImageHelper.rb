9852d10e2c6fa9215e76e53151e82d1a489d5c4e
Trond Lossius <trond.lossius@bek.no>
2013-01-05 12:30:49 +0100

    Adding new fancyImage helper, and a demo page illustrating how to use it. Once blog posts are updated to use this helper, it closes #1281

--- lib/fancyImageHelper.rb
+++ lib/fancyImageHelper.rb
@@ -1,5 +1,26 @@
 module FancyImageHelper
-  def fancyImage(item)
-    fancyString = "<a class=\"fancybox\" href=\"" + @item.identifier + "images/jamoma-frameworks.png\" data-fancybox-group=\"fancybox\" title=\"YAML project format\"> <img alt=\"YAML project format\" class=\"myCell\" src=\"" + @item.identifier + "images/jamoma-frameworks.png\"/></a>"
+  def fancyImage(item, image, thumb="useImage", caption="useImageTitle", context="fancybox", effect="normal")
+    
+    if (thumb=="useImage")
+      thumb = image
+    end
+    
+    if (caption=="useImageTitle")
+      caption = image
+    end
+    
+    if (effect=="effect-a")
+      effect = "fancybox-effects-a"
+    elsif (effect=="effect-b")
+      effect = "fancybox-effects-b"
+    elsif (effect=="effect-c")
+      effect = "fancybox-effects-c"
+    elsif (effect=="effect-d")
+      effect = "fancybox-effects-d"
+    else
+      effect = "fancybox"
+    end
+      
+    fancyString = fancyString = "<a class=\"" + effect + "\" href=\"" + @item.identifier + image + "\"data-fancybox-group=\"" + context +"\" title=\"" + caption + "\"> <img alt=\"" + caption + "\" class=\"myCell\" src=\"" + @item.identifier + thumb + "\"/></a>"
   end
 end
