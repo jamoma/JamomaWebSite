7e0d1b3b24f3e11b05cd15ccf609ddadbc496f20
Trond Lossius <trond.lossius@bek.no>
2016-03-15 14:05:22 +0100

    Improving layout for blog categories pages - Closes #43

--- lib/categories.rb
+++ lib/categories.rb
@@ -90,7 +90,7 @@ module Categories
       @items << Nanoc::Item.new(
           "<%= render('category', :category => '#{category}') %>",
           {
-              :title => "Posts in the #{category}",
+              :title => "Posts <span class=\"text-lowercase\">in the <span class=\"text-primary\"><strong>#{category}</strong></span> category</span>",
               :h1 => "#{category} posts",
               :menu => "blog",
               :posts => posts
